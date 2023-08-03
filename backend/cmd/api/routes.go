24d785debcce0232e7fea2f9cde1f2940fd12a1a
Clarenth <standadit.dev@protonmail.com>
2023-08-03 06:32:12 -0400

    Created routes file

--- backend/cmd/api/routes.go
+++ backend/cmd/api/routes.go
@@ -4,10 +4,12 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func (app *application) routes() http.Handler {
 	mux := chi.NewRouter()
+	mux.Use(middleware.Recoverer)
 
 	return mux
 }
