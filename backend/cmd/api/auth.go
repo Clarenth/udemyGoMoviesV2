2914d5ce46f183b4d29ad3803b05fafc8805432c
Clarenth <standadit.dev@protonmail.com>
2023-08-28 19:58:46 -0400

    Created jwt GenerateTokenPair func, jwt token claims, jwt expiry time, signedAccessToken, and started refreshToken

--- backend/cmd/api/auth.go
+++ backend/cmd/api/auth.go
@@ -1,20 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
 )
 
 type Auth struct {
-	Issuer         string
-	Audience       string
-	Secret         string
-	TokenExpiery   time.Duration
-	RefreshExpiery time.Duration
-	CookieDomain   string
-	CookiePath     string
-	CookieName     string
+	Issuer        string
+	Audience      string
+	Secret        string
+	TokenExpiry   time.Duration
+	RefreshExpiry time.Duration
+	CookieDomain  string
+	CookiePath    string
+	CookieName    string
 }
 
 type jwtUser struct {
@@ -30,4 +31,39 @@ type TokenPairs struct {
 
 type Claims struct {
 	jwt.RegisteredClaims
+}
+
+// Generates the JWT and Refresh pair
+func (j *Auth) GenerateTokenPair(user *jwtUser) (TokenPairs, error) {
+	// Create a token
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	// Set the claims of the token
+	claims := token.Claims.(jwt.MapClaims)
+	claims["name"] = fmt.Sprintf("%s %s", user.FirstName, user.LastName)
+	claims["sub"] = fmt.Sprint(user.ID)
+	claims["aud"] = j.Audience
+	claims["iss"] = j.Issuer
+	claims["iat"] = time.Now().UTC().Unix()
+	claims["typ"] = "JWT"
+
+	// Set the expiry for the JWT
+	claims["exp"] = time.Now().UTC().Add(j.TokenExpiry).Unix()
+
+	// Create the signed token
+	signedAccessToken, err := token.SignedString([]byte(j.Secret))
+	if err != nil {
+		return TokenPairs{}, err
+	}
+
+	// Create the refresh token and the claims
+	refreshToken := jwt.New(jwt.SigningMethodHS256)
+
+	// Set the expiery of the refresh token
+
+	// Create the signed refresh token
+
+	// Create the token pairs and populate with signed tokens
+
+	// Return the token pairs
 }
